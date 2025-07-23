e010de97d1f5ba01f02ed4e6aa9f42642a3548bc
Saif-Dawood <saifdawoodcs@gmail.com>
2025-07-23 12:00:58 +0300

    [ADD] TaskList Component, TaskList Service

---
+++ src/app/task-list/task-list.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, inject, input } from '@angular/core';
+import { TasksService } from './task-list.service';
+import { Task } from './task-item/task-item.model';
+import { ResolveFn, RouterLink, RouterOutlet } from '@angular/router';
+import { TaskItemComponent } from "./task-item/task-item.component";
+
+@Component({
+    selector: 'app-task-list',
+    standalone: true,
+    imports: [RouterLink, TaskItemComponent, RouterOutlet],
+    templateUrl: './task-list.component.html',
+    styleUrl: './task-list.component.css'
+})
+export class TaskListComponent implements OnInit {
+    tasksService = inject(TasksService);
+    order = input<'asc' | 'desc' | undefined>();
+
+    ngOnInit(): void {
+        console.log("ngOnInit");
+        this.tasksService.fetchTasks();
+        console.log(this.tasksService.allTasks);
+    }
+
+}
+
+export const resolveTasks: ResolveFn<Task[]> = (
+    activatedRouteSnapshot,
+    routerState
+) => {
+    console.log("resolveTasks() is currently running")
+    const order = activatedRouteSnapshot.queryParams['order'];
+    const tasksService = inject(TasksService);
+    const tasks = tasksService.allTasks()
+
+    if (order && order === 'asc') {
+        tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
+    } else {
+        tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
+    }
+
+    return tasks.length ? tasks : [];
+};
---
+++ src/app/task-list/task-list.service.ts
@@ -0,0 +1,68 @@
+import { DestroyRef, Injectable, inject, signal } from '@angular/core';
+import { NewTaskData, Priority, Status, Task } from './task-item/task-item.model';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({ providedIn: 'root' })
+export class TasksService {
+    private jsonUrl = 'assets/mock-tasks.json';
+    private http = inject(HttpClient);
+    private tasks = signal<Task[]>([]);
+    private destroyRef = inject(DestroyRef);
+
+    allTasks = this.tasks.asReadonly();
+
+
+    fetchTasks() {
+        const tasks = localStorage.getItem('tasks');
+
+        if (tasks) {
+            this.tasks.set(JSON.parse(tasks));
+            return;
+        }
+
+        const subscription = this.http.get<Task[]>(this.jsonUrl).subscribe(data => {
+            this.tasks.set(
+                data.map((task) => ({
+                    id: task.id,
+                    title: task.title,
+                    description: task.description,
+                    dueDate: task.dueDate,
+                    status: task.status as Status,
+                    priority: task.priority as Priority,
+                }))
+            );
+        });
+        this.destroyRef.onDestroy(() => {
+            subscription.unsubscribe();
+        });
+    }
+
+    addTask(taskData: NewTaskData) {
+        const lastTask  = this.allTasks().at(-1);
+        this.tasks.update((prevTasks) => [
+            {
+                id: lastTask ? lastTask.id + 1 : 0,
+                title: taskData.title,
+                description: taskData.description,
+                dueDate: taskData.dueDate,
+                status: taskData.status,
+                priority: taskData.priority,
+            },
+            ...prevTasks,
+        ]);
+        this.saveTasks();
+    }
+
+    removeTask(id: number) {
+        this.tasks.update((prevTasks) =>
+            prevTasks.filter((task) => task.id !== id)
+        );
+        this.saveTasks();
+    }
+
+    private saveTasks() {
+        localStorage.setItem('tasks', JSON.stringify(this.tasks()));
+    }
+
+}
