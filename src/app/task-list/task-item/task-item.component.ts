63f4def177e505a1ccd20a7094f4491054aba350
Saif-Dawood <saifdawoodcs@gmail.com>
2025-07-23 11:59:39 +0300

    [ADD] TaskItem Component

---
+++ src/app/task-list/task-item/task-item.component.ts
@@ -0,0 +1,30 @@
+import { Component, inject, input } from '@angular/core';
+import { Task } from './task-item.model';
+import { TasksService } from '../task-list.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CardComponent } from "../../shared/card/card.component";
+import { DatePipe, TitleCasePipe } from '@angular/common';
+
+@Component({
+    selector: 'app-task-item',
+    standalone: true,
+    imports: [CardComponent, DatePipe, TitleCasePipe],
+    templateUrl: './task-item.component.html',
+    styleUrl: './task-item.component.css'
+})
+export class TaskItemComponent {
+    task = input.required<Task>();
+    private tasksService = inject(TasksService);
+    private router = inject(Router);
+    private activatedRoute = inject(ActivatedRoute);
+
+    onComplete() {
+        this.tasksService.removeTask(this.task().id);
+        this.router.navigate(['.'], {
+            relativeTo: this.activatedRoute,
+            onSameUrlNavigation: 'reload',
+            queryParamsHandling: 'preserve',
+        });
+    }
+}
+
