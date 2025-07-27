663efbcf657efd89bd349f4e432e88381e4ed279
Saif-Dawood <saifdawoodcs@gmail.com>
2025-07-27 17:08:37 +0300

    [UPDT] TaskForm Component: ADD Validators

--- src/app/task-list/task-form/task-form.component.ts
+++ src/app/task-list/task-form/task-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, input, signal } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, CanDeactivateFn, Router, RouterLink } from '@angular/router';
 
 import { TasksService } from '../task-list.service';
@@ -23,30 +23,46 @@ export class TaskFormComponent {
     submitText: string = 'Create';
 
     ngOnInit() {
+        this.taskForm = new FormGroup({
+            title: new FormControl('', [
+                Validators.required,
+                Validators.minLength(3)
+            ]),
+            description: new FormControl('', [
+                Validators.required,
+                Validators.minLength(10)
+            ]),
+            status: new FormControl<Status>('To Do', [
+                Validators.required
+            ]),
+            priority: new FormControl<Priority>('P4', [
+                Validators.required
+            ]),
+            dueDate: new FormControl('', [
+                Validators.required,
+                (control) => {
+                    const selectedDate = new Date(control.value);
+                    const now = new Date();
+                    return selectedDate > now ? null : { dateInvalid: true };
+                }
+            ]),
+        });
         const id = Number(this.activatedRoute.snapshot.paramMap.get('id'));
 
         if (id){
             const task = this.tasksService.filterById(id);
             if (task) {
-                this.taskForm = new FormGroup({
-                    title: new FormControl(task.title),
-                    description: new FormControl(task.description),
-                    status: new FormControl<Status>(task.status),
-                    priority: new FormControl<Priority>(task.priority),
-                    dueDate: new FormControl(task.dueDate),
+                this.taskForm.setValue({
+                    title: task.title,
+                    description: task.description,
+                    status: task.status,
+                    priority: task.priority,
+                    dueDate: task.dueDate,
                 });
                 this.id = id;
                 this.submitText = 'Edit';
-                return;
             }
         }
-        this.taskForm = new FormGroup({
-            title: new FormControl(''),
-            description: new FormControl(''),
-            status: new FormControl<Status>('To Do'),
-            priority: new FormControl<Priority>('P1'),
-            dueDate: new FormControl(''),
-        });
     }
   
     onSubmit() {
@@ -64,6 +80,9 @@ export class TaskFormComponent {
             this.router.navigate(['/tasks'], {
                 replaceUrl: true,
             })
+        }
+        else {
+            console.log(this.taskForm);
         }
     }
   
